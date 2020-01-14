70ceecf0f989151c7bdf36ae1ea2a79abf900e85
Michael Burman <yak@iki.fi>
2020-01-14 12:06:29 +0200

    CPOL-29 Let Quarkus configure the logging implementation

--- commons/src/main/java/org/hawkular/commons/log/MsgLogger.java
+++ commons/src/main/java/org/hawkular/commons/log/MsgLogger.java
@@ -1,16 +1,8 @@
 package org.hawkular.commons.log;
 
-
 import org.jboss.logging.BasicLogger;
 import org.jboss.logging.annotations.MessageLogger;
 
-/**
- * Default Logger.
- * Each project can extend it with personalized messages.
- *
- * @author Jay Shaughnessy
- * @author Lucas Ponce
- */
-@MessageLogger(projectCode = "HAWKULAR")
+@MessageLogger(projectCode = "CPOL")
 public interface MsgLogger extends BasicLogger {
 }
