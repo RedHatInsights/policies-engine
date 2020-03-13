d32780c53aee47f227c56134b408cfa9808a9490
Heiko W. Rupp <hrupp@redhat.com>
2020-03-13 12:47:27 +0100

    Add some instrumentation to support distributed tracing. For production, I guess the sample rate should be reduced from 1 to 0.1 once Kafka messages are also traced.

---
+++ external/src/main/java/com/redhat/cloud/custompolicies/engine/TracerStartup.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2020 Red Hat, Inc. and/or its affiliates
+ * and other contributors as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.redhat.cloud.custompolicies.engine;
+
+import io.opentracing.Tracer;
+import io.opentracing.contrib.vertx.ext.web.TracingHandler;
+import io.vertx.ext.web.Router;
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Observes;
+import javax.inject.Inject;
+
+/**
+ * Set up distributed tracing
+ * @author hrupp
+ */
+@ApplicationScoped
+public class TracerStartup {
+
+  @Inject
+  Tracer tracer;
+
+  @PostConstruct
+  public void init(@Observes Router router) {
+
+    TracingHandler handler = new TracingHandler(tracer);
+    router.route()
+        .order(-1).handler(handler)
+        .failureHandler(handler);
+  }
+}
