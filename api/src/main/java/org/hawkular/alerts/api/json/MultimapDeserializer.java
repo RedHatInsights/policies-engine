ac3cf7c0a3e3a31281f5d89ec6cc58de3f64c443
Michael Burman <yak@iki.fi>
2020-09-22 16:08:24 +0300

    Change tag serialization format to [{},{}]

--- api/src/main/java/org/hawkular/alerts/api/json/MultimapDeserializer.java
+++ api/src/main/java/org/hawkular/alerts/api/json/MultimapDeserializer.java
@@ -2,34 +2,38 @@ package org.hawkular.alerts.api.json;
 
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.JsonToken;
 import com.fasterxml.jackson.core.ObjectCodec;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.google.common.collect.LinkedHashMultimap;
 import com.google.common.collect.Multimap;
 
 import java.io.IOException;
 import java.util.Iterator;
+import java.util.Map;
 
 /**
  * Deserializes a map structure with multiple same keys to a multimap. Does not use jackson-datatype-guava package, because the layout
  * it provides is {"key": ["value", "value2"]} while we do not want to break the compatibility to existing data. As such, we allow the
- * input to be in the format of: [{"key": "value"}, {"key": "value2"}]. For convenience, that same struct is used in the insights reports
- * also providing better inter compatibility with other insights applications.
+ * input to be in the format of: [{"key": "value"}, {"key": "value2"}].
  */
 public class MultimapDeserializer extends JsonDeserializer<Multimap<String, String>> {
     @Override
     public Multimap<String, String> deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException, JsonProcessingException {
-        ObjectCodec objectCodec = jsonParser.getCodec();
-        JsonNode mapNode = objectCodec.readTree(jsonParser);
-
         LinkedHashMultimap<String, String> map = LinkedHashMultimap.create();
-        Iterator<String> fieldNames = mapNode.fieldNames();
-        while (fieldNames.hasNext()) {
-            String field = fieldNames.next();
-            map.put(field, mapNode.get(field).asText());
+
+        ObjectCodec objectCodec = jsonParser.getCodec();
+        JsonNode node = objectCodec.readTree(jsonParser);
+
+        if (node.isArray()) {
+            node.forEach(entry -> {
+                entry.fields().forEachRemaining(entryInner -> map.put(entryInner.getKey(), entryInner.getValue().asText()));
+            });
         }
+
         return map;
     }
 }
