476d83ce31b2c6d6de6aa82eaf1b9b74357fd9a3
Bennet Schulz <mail@bennet-schulz.de>
2021-06-10 13:42:32 +0200

    POL-502 Add silenced logs

--- external/src/main/java/com/redhat/cloud/policies/engine/AlertStarter.java
+++ external/src/main/java/com/redhat/cloud/policies/engine/AlertStarter.java
@@ -3,6 +3,9 @@ package com.redhat.cloud.policies.engine;
 import com.redhat.cloud.policies.engine.actions.QuarkusActionPluginRegister;
 import io.quarkus.runtime.ShutdownEvent;
 import io.quarkus.runtime.StartupEvent;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.hawkular.alerts.AlertsStandalone;
 import org.hawkular.alerts.api.services.ActionsService;
 import org.hawkular.alerts.api.services.AlertsService;
@@ -21,8 +24,15 @@ import static org.hawkular.alerts.api.util.Util.isEmpty;
 
 @Singleton
 public class AlertStarter {
+
     public static MsgLogger LOGGER = MsgLogging.getMsgLogger(AlertStarter.class);
     private final static String BUILD_COMMIT_ENV_NAME = "OPENSHIFT_BUILD_COMMIT";
+
+    public static final String FILTER_REGEX = ".*(/health(/\\w+)?|/metrics) HTTP/[0-9].[0-9]\" 200.*\\n?";
+    private static final Pattern pattern = Pattern.compile(FILTER_REGEX);
+
+    @ConfigProperty(name = "quarkus.http.access-log.category")
+    String loggerName;
 
     @Inject
     AlertsStandalone alerts;
@@ -55,6 +65,7 @@ public class AlertStarter {
         } else {
             LOGGER.info("Starting Policies Engine.");
         }
+        initAccessLogFilter();
         initialize();
     }
 
@@ -76,5 +87,14 @@ public class AlertStarter {
                         LOGGER.info("Started Policies Engine");
                     }
         }).join();
+    }
+
+    private void initAccessLogFilter() {
+        java.util.logging.Logger accessLog = java.util.logging.Logger.getLogger(loggerName);
+        accessLog.setFilter(record -> {
+            final String logMessage = record.getMessage();
+            Matcher matcher = pattern.matcher(logMessage);
+            return !matcher.matches();
+        });
     }
 }
