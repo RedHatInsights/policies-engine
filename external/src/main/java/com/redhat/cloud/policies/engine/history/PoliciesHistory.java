ad985a894001cf38539e503c6d900096dbb2fe54
Gwenneg Lepage <gwenneg@users.noreply.github.com>
2021-12-01 16:59:09 +0100

    POL-560 Remove policies history feature flag (#283)

--- external/src/main/java/com/redhat/cloud/policies/engine/history/PoliciesHistory.java
+++ external/src/main/java/com/redhat/cloud/policies/engine/history/PoliciesHistory.java
@@ -1,7 +1,5 @@
 package com.redhat.cloud.policies.engine.history;
 
-import io.quarkus.runtime.StartupEvent;
-import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.hawkular.alerts.api.model.event.Alert;
 import org.hawkular.alerts.api.services.PoliciesHistoryService;
 import org.hibernate.Session;
@@ -9,7 +7,6 @@ import org.jboss.logging.Logger;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.context.control.ActivateRequestContext;
-import javax.enterprise.event.Observes;
 import javax.inject.Inject;
 import javax.transaction.Transactional;
 import java.util.Iterator;
@@ -22,38 +19,25 @@ public class PoliciesHistory implements PoliciesHistoryService {
     private static final String INVENTORY_ID_TAG_KEY = "inventory_id";
     private static final String DISPLAY_NAME_TAG_KEY = "display_name";
 
-    @ConfigProperty(name = "engine.policies-history.enabled", defaultValue = "true")
-    boolean enabled;
-
     @Inject
     Session session;
-
-    void logAtStartup(@Observes StartupEvent event) {
-        if (enabled) {
-            LOGGER.info("Policies history is enabled. The history will be persisted into the database.");
-        } else {
-            LOGGER.info("Policies history is disabled. The history won't be persisted into the database.");
-        }
-    }
 
     @Override
     @Transactional
     @ActivateRequestContext
     public void put(Alert alert) {
-        if (enabled) {
-            try {
-                LOGGER.debugf("Creating %s from %s", PoliciesHistoryEntry.class.getSimpleName(), alert);
-                PoliciesHistoryEntry historyEntry = new PoliciesHistoryEntry();
-                historyEntry.setTenantId(alert.getTenantId());
-                historyEntry.setPolicyId(alert.getTriggerId());
-                historyEntry.setCtime(alert.getCtime());
-                getSingleTagValue(alert, INVENTORY_ID_TAG_KEY).ifPresent(historyEntry::setHostId);
-                getSingleTagValue(alert, DISPLAY_NAME_TAG_KEY).ifPresent(historyEntry::setHostName);
-                session.persist(historyEntry);
-                LOGGER.debugf("Persisted %s", historyEntry);
-            } catch (Exception e) {
-                LOGGER.error("Policies history entry persist failed", e);
-            }
+        try {
+            LOGGER.debugf("Creating %s from %s", PoliciesHistoryEntry.class.getSimpleName(), alert);
+            PoliciesHistoryEntry historyEntry = new PoliciesHistoryEntry();
+            historyEntry.setTenantId(alert.getTenantId());
+            historyEntry.setPolicyId(alert.getTriggerId());
+            historyEntry.setCtime(alert.getCtime());
+            getSingleTagValue(alert, INVENTORY_ID_TAG_KEY).ifPresent(historyEntry::setHostId);
+            getSingleTagValue(alert, DISPLAY_NAME_TAG_KEY).ifPresent(historyEntry::setHostName);
+            session.persist(historyEntry);
+            LOGGER.debugf("Persisted %s", historyEntry);
+        } catch (Exception e) {
+            LOGGER.error("Policies history entry persist failed", e);
         }
     }
 
