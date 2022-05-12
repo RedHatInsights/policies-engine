eae41a496319b452f17c5fc4d03fae2024a15d15
Gwenneg Lepage <glepage@redhat.com>
2022-05-12 12:12:35 +0200

    Add default latest update

--- external/src/main/java/com/redhat/cloud/policies/engine/lightweight/TriggersLoader.java
+++ external/src/main/java/com/redhat/cloud/policies/engine/lightweight/TriggersLoader.java
@@ -2,10 +2,12 @@ package com.redhat.cloud.policies.engine.lightweight;
 
 import io.quarkus.cache.CacheResult;
 import org.hawkular.alerts.api.model.trigger.FullTrigger;
+import org.jboss.logging.Logger;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
@@ -15,6 +17,8 @@ import static java.util.stream.Collectors.toList;
 
 @ApplicationScoped
 public class TriggersLoader {
+
+    private static final Logger LOGGER = Logger.getLogger(TriggersLoader.class);
 
     private static class AccountTriggers {
         private LocalDateTime latestUpdate;
@@ -38,19 +42,27 @@ public class TriggersLoader {
 
     @CacheResult(cacheName = "account-latest-update")
     LocalDateTime findLatestUpdate(String accountId) {
+        LOGGER.debugf("Finding latest triggers update for account %s", accountId);
         String hql = "SELECT latest FROM AccountLatestUpdate WHERE accountId = :accountId";
-        return entityManager.createQuery(hql, LocalDateTime.class)
-                .setParameter("accountId", accountId)
-                .getSingleResult();
+        try {
+            return entityManager.createQuery(hql, LocalDateTime.class)
+                    .setParameter("accountId", accountId)
+                    .getSingleResult();
+        } catch (NoResultException e) {
+            LOGGER.debugf("No latest triggers update found for account %s", accountId);
+            return LocalDateTime.MIN;
+        }
     }
 
     private List<FullTrigger> loadTriggersByAccount(String accountId) {
         String hql = "SELECT p FROM Policy p WHERE p.accountId = :accountId";
-        return entityManager.createQuery(hql, Policy.class)
+        List<FullTrigger> triggers = entityManager.createQuery(hql, Policy.class)
                 .setParameter("accountId", accountId)
                 .getResultList()
                 .stream()
                 .map(PolicyToTriggerConverter::convert)
                 .collect(toList());
+        LOGGER.debugf("%d database trigger(s) loaded for account %s", triggers.size(), accountId);
+        return triggers;
     }
 }
