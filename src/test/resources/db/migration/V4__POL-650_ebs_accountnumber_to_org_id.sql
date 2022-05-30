c8b63423d3d2faecbc4ed1695c98748cf5cee29a
Bennet Schulz <mail@bennet-schulz.de>
2022-05-30 14:52:28 +0200

    POL-650 Add colum for policy

--- src/test/resources/db/migration/V4__POL-650_ebs_accountnumber_to_org_id.sql
+++ src/test/resources/db/migration/V4__POL-650_ebs_accountnumber_to_org_id.sql
@@ -3,3 +3,8 @@ CREATE TABLE org_id_latest_update (
     latest TIMESTAMP NOT NULL,
     CONSTRAINT pk_org_id_latest_update PRIMARY KEY (org_id)
 );
+
+ALTER TABLE policy
+  ADD COLUMN org_id text;
+
+CREATE INDEX ix_policy_org_id ON public.policy (org_id);
