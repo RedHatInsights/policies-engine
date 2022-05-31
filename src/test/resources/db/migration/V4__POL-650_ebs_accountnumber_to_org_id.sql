aef5efe324f21de0d0d638336c919f6c16a4c3c4
Bennet Schulz <mail@bennet-schulz.de>
2022-05-31 14:14:12 +0200

    POL-650 Add orgId to PoliciesHistory classes

--- src/test/resources/db/migration/V4__POL-650_ebs_accountnumber_to_org_id.sql
+++ src/test/resources/db/migration/V4__POL-650_ebs_accountnumber_to_org_id.sql
@@ -8,3 +8,8 @@ ALTER TABLE policy
   ADD COLUMN org_id text;
 
 CREATE INDEX ix_policy_org_id ON public.policy (org_id);
+
+ALTER TABLE policies_history
+  ADD COLUMN org_id text;
+
+CREATE INDEX ix_policies_history_org_id ON public.policies_history (org_id);
